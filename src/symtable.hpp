856b728227abcf5052b337af5f794330f77ca477
dmitryg <dmitrygrigorev2005@gmail.com>
2026-04-01 03:28:10 +0300

    missing files

---
+++ src/symtable.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+struct VariableInfo {
+  std::string name;
+  std::string type;
+};
+
+struct MethodInfo {
+  std::string name;
+  std::string return_type;
+  std::vector<VariableInfo> arguments;
+};
+
+struct ClassInfo {
+  std::string name;
+  std::unordered_map<std::string, VariableInfo> fields;
+  std::unordered_map<std::string, MethodInfo> methods;
+};
+
+struct GlobalSymbolTable {
+  std::unordered_map<std::string, ClassInfo> classes;
+  std::unordered_map<std::string, MethodInfo> global_methods;
+};
+
+class Scope {
+ public:
+  Scope(Scope* parent_def, GlobalSymbolTable* gst)
+      : parent(parent_def), global_sym_table(gst) {}
+
+  bool DeclareVariable(const std::string& name, const std::string& type);
+  VariableInfo* ResolveVariable(const std::string& name);
+
+ public:
+  Scope* parent;
+  std::vector<std::unique_ptr<Scope>> children;
+  std::unordered_map<std::string, VariableInfo> local_variables;
+  GlobalSymbolTable* global_sym_table;
+};
